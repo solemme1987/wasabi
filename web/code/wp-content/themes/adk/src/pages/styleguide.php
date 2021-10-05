d57c09a6d0688aeeb5311196d7bb4ddc76003e5c
Ricardo Cortes Hurtado <rcortes@adkgroup.com>
2021-10-04 22:49:46 -0500

    [WSBI-GLOBAL]style guide page created

---
+++ web/code/wp-content/themes/adk/src/pages/styleguide.php
@@ -0,0 +1,5 @@
+<section class="c-styleguide">
+  <div class="o-container">
+   <h1>GUIA DE ESTILOS Ricardo</h1>
+  </div>
+</section>
---
+++ web/code/wp-content/themes/adk/styleguide.php
@@ -0,0 +1,5 @@
+<?php // Template Name: styleguide //
+$pagename = "styleguide"; //match js and scss name;
+assemble_template($pagename);
+
+?>
