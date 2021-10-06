6c0a9410916f19b93ab894a2c1a3c194aeea3ea5
Ricardo Cortes Hurtado <rcortes@adkgroup.com>
2021-10-06 00:36:02 -0500

    [WSBI-GLOBAL]Colors and gradient finished

--- web/code/wp-content/themes/adk/src/pages/styleguide.php
+++ web/code/wp-content/themes/adk/src/pages/styleguide.php
@@ -1,5 +1,163 @@
 <section class="c-styleguide">
-  <div class="o-container">
-   <h1>GUIA DE ESTILOS Ricardo</h1>
+  <div class="o-container o-container--std">
+
+    <div class="o-grid o-grid--small u-push-1/12 u-push-0@md">
+      <div class="o-grid__col u-12/12">
+        <h1 class="c-styleguide__title">Colors and Text Styles</h1>
+      </div>
+      <div class="o-grid__col u-12/12">
+        <h2 class="c-styleguide__subtitle">Color / Fills</h2>
+      </div>
+    </div>
+
+    <!-- -----------------------------
+    ----------------------------------
+               COLORS 
+    --------------------------------->
+    <div class="o-grid o-grid--small u-push-1/12 u-push-0@md">
+      <!-- WHITE COLOR -->
+      <div class="o-grid__col u-11/12 u-5/12@sm u-3/12@md ">
+        <div class="c-styleguide__card">
+          <div class="c-styleguide__card-body"></div>
+          <h2 class="c-styleguide__card-title">White</h2>
+          <p class="c-styleguide__card-hexa">#FFFFFF</p>
+          <p class="c-styleguide__card-rgba">rgba(255, 255, 255, 1)</p>
+        </div>
+      </div>
+
+      <!-- BLACK COLOR -->
+      <div class="o-grid__col u-11/12 u-5/12@sm u-3/12@md ">
+        <div class="c-styleguide__card">
+          <div class="c-styleguide__card-body  c-styleguide__card-body--black"></div>
+          <h2 class="c-styleguide__card-title">Black</h2>
+          <p class="c-styleguide__card-hexa">#111111</p>
+          <p class="c-styleguide__card-rgba">rgba(17, 17, 17, 1)</p>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="o-grid o-grid--small u-push-1/12 u-push-0@md">
+
+      <!-- GREEN COLOR -->
+      <div class="o-grid__col u-11/12 u-5/12@sm u-3/12@md ">
+        <div class="c-styleguide__card">
+          <div class="c-styleguide__card-body  c-styleguide__card-body--green"></div>
+          <h2 class="c-styleguide__card-title">Green</h2>
+          <p class="c-styleguide__card-hexa">#36B32F</p>
+          <p class="c-styleguide__card-rgba">rgba(54, 179, 47, 1)</p>
+        </div>
+      </div>
+
+      <!-- DARK BLUE COLOR -->
+      <div class="o-grid__col u-11/12 u-5/12@sm u-3/12@md ">
+        <div class="c-styleguide__card">
+          <div class="c-styleguide__card-body  c-styleguide__card-body--dark-blue"></div>
+          <h2 class="c-styleguide__card-title">Dark Blue</h2>
+          <p class="c-styleguide__card-hexa">#061F32</p>
+          <p class="c-styleguide__card-rgba">rgba(6, 31, 50 , 1)</p>
+        </div>
+      </div>
+
+      <!-- PRIMARY BLUE COLOR -->
+      <div class="o-grid__col u-11/12 u-5/12@sm u-3/12@md ">
+        <div class="c-styleguide__card">
+          <div class="c-styleguide__card-body  c-styleguide__card-body--primary-blue"></div>
+          <h2 class="c-styleguide__card-title">Primary Blue</h2>
+          <p class="c-styleguide__card-hexa">#054A91</p>
+          <p class="c-styleguide__card-rgba">rgba(5, 74, 145, 1)</p>
+        </div>
+      </div>
+
+      <!-- LIGHT BLUE COLOR -->
+      <div class="o-grid__col u-11/12 u-5/12@sm u-3/12@md ">
+        <div class="c-styleguide__card">
+          <div class="c-styleguide__card-body  c-styleguide__card-body--light-blue"></div>
+          <h2 class="c-styleguide__card-title">Light Blue</h2>
+          <p class="c-styleguide__card-hexa">#0064E9</p>
+          <p class="c-styleguide__card-rgba">rgba(100, 233, 145, 1)</p>
+        </div>
+      </div>
+
+      <!-- DARK GRAY COLOR -->
+      <div class="o-grid__col u-11/12 u-5/12@sm u-3/12@md ">
+        <div class="c-styleguide__card">
+          <div class="c-styleguide__card-body  c-styleguide__card-body--dark-gray"></div>
+          <h2 class="c-styleguide__card-title">Dark Gray</h2>
+          <p class="c-styleguide__card-hexa">#E3E3E3</p>
+          <p class="c-styleguide__card-rgba">rgba(227, 227, 227, 1)</p>
+        </div>
+      </div>
+
+      <!-- BASE GRAY COLOR -->
+      <div class="o-grid__col u-11/12 u-5/12@sm u-3/12@md  ">
+        <div class="c-styleguide__card">
+          <div class="c-styleguide__card-body  c-styleguide__card-body--base-gray"></div>
+          <h2 class="c-styleguide__card-title">Base Gray</h2>
+          <p class="c-styleguide__card-hexa">#EFEFEF</p>
+          <p class="c-styleguide__card-rgba">rgba(239, 239, 239, 1)</p>
+        </div>
+      </div>
+
+      <!-- LIGHT GRAY COLOR -->
+      <div class="o-grid__col u-11/12 u-5/12@sm u-3/12@md ">
+        <div class="c-styleguide__card">
+          <div class="c-styleguide__card-body  c-styleguide__card-body--base-gray"></div>
+          <h2 class="c-styleguide__card-title">Light Gray</h2>
+          <p class="c-styleguide__card-hexa">#F8F8F8</p>
+          <p class="c-styleguide__card-rgba">rgba(248, 248, 248, 1)</p>
+        </div>
+      </div>
+
+    </div>
+
+    <!-- -----------------------------
+    ----------------------------------
+              END COLORS 
+    --------------------------------->
+    
+
+    <!-- -----------------------------
+    ----------------------------------
+               BORDERS
+    --------------------------------->
+    <div class="o-grid o-grid--small u-push-1/12 u-push-0@md">
+      <div class="o-grid__col u-12/12">
+        <h2 class="c-styleguide__subtitle">Borders</h2>
+      </div>
+    </div>
+
+    <div class="o-grid o-grid--small u-push-1/12 u-push-0@md">
+      <div class="o-grid__col u-11/12 u-5/12@sm u-3/12@md ">
+        <div class="c-styleguide__card">
+          <div class="c-styleguide__card-body"></div>
+          <h2 class="c-styleguide__card-title">Gray width 1 </h2>
+          <p class="c-styleguide__card-hexa">#E4E4E4</p>
+          <p class="c-styleguide__card-rgba">rgba(228, 228, 228, 1)</p>
+        </div>
+      </div>
+    </div>
+
+     <!-- -----------------------------
+    ----------------------------------
+               GRADIENTS
+    --------------------------------->
+    <div class="o-grid o-grid--small u-push-1/12 u-push-0@md">
+      <div class="o-grid__col u-12/12">
+        <h2 class="c-styleguide__subtitle">Gradients</h2>
+      </div>
+    </div>
+
+    <div class="o-grid o-grid--small u-push-1/12 u-push-0@md">
+      <div class="o-grid__col u-11/12 u-5/12@sm u-3/12@md ">
+        <div class="c-styleguide__card">
+          <div class="c-styleguide__card-body c-styleguide__card-body--gradient"></div>
+          <h2 class="c-styleguide__card-title">Dark Blue to light</h2>
+          <p class="c-styleguide__card-hexa">#03101B - 003A68 </p>
+        </div>
+      </div>
+    </div>
+
+
   </div>
 </section>
